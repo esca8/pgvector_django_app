85e8a079bd679809c3f6677c8b7e0616fab8adf9
fc <fc@fcs-MacBook-Air.local>
2024-09-30 02:02:56 -0400

    load openai key from env

--- blog/embedding.py
+++ blog/embedding.py
@@ -2,7 +2,7 @@ import os
 import openai
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) 
+load_dotenv(find_dotenv()) 
 openai.api_key  = os.environ['OPENAI_API_KEY'] 
 
 def get_embeddings(text):
